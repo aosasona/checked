e890b0ffdc21049a87bed4ecece4dc95c9599522
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2023-06-03 23:41:57 +0100

    initial commit

---
+++ checked/checkedApp.swift
@@ -0,0 +1,17 @@
+//
+//  checkedApp.swift
+//  checked
+//
+//  Created by Ayodeji Osasona on 03/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct checkedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
