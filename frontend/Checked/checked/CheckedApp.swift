7768c1a254b02c6613d76dfed2ea018af395b0c7
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2023-06-04 06:33:35 +0100

    added firebase

---
+++ frontend/Checked/checked/CheckedApp.swift
@@ -0,0 +1,38 @@
+import SwiftUI
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseAuth
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        
+        // only use firebase- emulator in dev
+        let useEmulator = UserDefaults.standard.bool(forKey: "useEmulator")
+        if useEmulator {
+            let settings = Firestore.firestore().settings
+            settings.host = "localhost:8080"
+            settings.isSSLEnabled = false
+            Firestore.firestore().settings = settings
+            
+            Auth.auth().useEmulator(withHost: "localhost", port: 9099)
+        }
+        
+        return true
+      }
+}
+
+@main
+struct CheckedApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            NavigationStack {
+                RemindersListView()
+                    .navigationTitle("Reminders")
+            }
+        }
+    }
+}
